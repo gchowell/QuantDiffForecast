f631f8ae0d09578ffd6fe5ae5741c4fd83fed6a9
gchowell <31742415+gchowell@users.noreply.github.com>
2023-09-01 15:23:59 -0400

    Add files via upload

--- forecasting_odemodels code/options_forecast_EXP_flu1918_dist1_3.m
+++ forecasting_odemodels code/options_forecast_EXP_flu1918_dist1_3.m
@@ -107,5 +107,3 @@ tstart1=1; % time point for the start of rolling window analysis
 tend1=1;  %time point for the end of the rolling window analysis
 
 printscreen1=0; % print outputs to screen (1) or not (0)
-
-options_forecast_EXP_flu1918_dist1_3
